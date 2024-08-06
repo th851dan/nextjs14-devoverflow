7c55b93cd7f3dffb07da8f23b056eda03a91a102
Thanh Son Dang <sondang3298_hp@yahoo.com.vn>
2024-08-06 17:57:31 +0200

    redirect /profile to loggedin User's profile

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
@@ -10,11 +11,15 @@ const isProtectedRoute = createRouteMatcher([
   "/collection(.*)",
   "/edit-answer/:id(.*)",
   "/profile/edit(.*)",
+  "/profile",
   "/question/edit/:id(.*)",
 ]);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
+  // When user accesses /profile redirects to login or to their own profile
+  if (auth().userId && req.nextUrl.pathname === "/profile")
+    return NextResponse.redirect(new URL(`/profile/${auth().userId}`, req.url));
 });
 
 export const config = {
