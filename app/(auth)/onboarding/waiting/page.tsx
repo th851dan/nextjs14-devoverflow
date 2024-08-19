49626d685c4ef08c8af07380dff69a2903222e6c
Thanh Son Dang <sondang3298_hp@yahoo.com.vn>
2024-08-19 15:23:36 +0200

    try to fix race condition bug: BO35 test in dev
    Mocking process of creating User in database takes 2000ms

---
+++ app/(auth)/onboarding/waiting/page.tsx
@@ -0,0 +1,45 @@
+"use client";
+import { useEffect, useState } from "react";
+import { redirect, useRouter } from "next/navigation";
+import { useAuth } from "@clerk/nextjs";
+
+const checkUserCreatedStatus = async (userId: any) => {
+  // Call an API endpoint to check if the user has been created in db
+  const response = await fetch(`/api/db/checkUserCreated?userId=${userId}`);
+  const { created } = await response.json();
+  return created;
+};
+
+const Page = () => {
+  const router = useRouter();
+  const { userId } = useAuth();
+  const [isUserCreated, setUserCreated] = useState(false);
+
+  useEffect(() => {
+    const timeout = setInterval(async () => {
+      const isCreated = await checkUserCreatedStatus(userId);
+      if (isCreated) {
+        console.log("Yeah");
+      } else {
+        console.log("No Why");
+      }
+      if (isCreated) {
+        setUserCreated(true);
+        clearInterval(timeout);
+      }
+    }, 5000); // Check every 5 seconds
+
+    return () => clearInterval(timeout); // Clean up interval on unmount
+  }, [router]);
+
+  useEffect(() => {
+    if (isUserCreated) {
+      console.log("Redirecting to onboarding...");
+      router.push("/onboarding");
+    }
+  }, [isUserCreated]);
+
+  return <div>Waiting for your account to be set up...</div>;
+};
+
+export default Page;
