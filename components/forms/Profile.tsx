c920a92d7d356c799dd5666b1447af01e21b7a03
Diep Bui <thb6229@thi.de>
2024-08-31 15:12:25 +0200

    BO-66: validation

--- components/forms/Profile.tsx
+++ components/forms/Profile.tsx
@@ -5,7 +5,7 @@ import { usePathname, useRouter } from "next/navigation";
 
 import * as z from "zod";
 import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
+// import { zodResolver } from "@hookform/resolvers/zod";
 
 import {
   Form,
