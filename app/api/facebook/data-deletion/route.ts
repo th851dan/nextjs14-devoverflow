283c6a50d1151eb439443aa22359a4fb9a4c4a4e
Thanh Son Dang <sondang3298_hp@yahoo.com.vn>
2024-09-05 17:11:51 +0200

    full address of hostname with logging

--- app/api/facebook/data-deletion/route.ts
+++ app/api/facebook/data-deletion/route.ts
@@ -88,9 +88,11 @@ export const POST = async (req: NextRequest, res: NextResponse) => {
       });
     }
 
-    console.log(`header 1: ${req.headers.get("host")}`);
-    console.log(`header 2: ${headers().get("host")}`);
-    const statusUrl = `${req.headers.get("host")}/api/users/deletion-status?id=${user?.id}`;
+    const protocol = req.headers.get("x-forwarded-proto") || "http";
+    const host = req.headers.get("host");
+    const fullUrl = `${protocol}://${host}`;
+    console.log("The hostname with full address: " + fullUrl);
+    const statusUrl = `${fullUrl}/api/users/deletion-status?id=${user?.id}`;
     const confirmationCode = user?.id; // Verwende die user_id als Bestätigungscode
 
     const responseData = {
