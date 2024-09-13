cdded22749f9ff5b80e118f7a3ff8c055e776968
Thanh Son Dang <sondang3298_hp@yahoo.com.vn>
2024-09-13 18:01:02 +0200

    define global Cookiebot var

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,8 +1,13 @@
-declare var Cookiebot: {
-  consent: {
-    necessary: boolean;
-    preferences: boolean;
-    statistics: boolean;
-    marketing: boolean;
-  };
-};
+declare global {
+  interface CookieConsent {
+    consent: {
+      necessary: boolean;
+      preferences: boolean;
+      statistics: boolean;
+      marketing: boolean;
+    };
+  }
+  var Cookiebot: CookieConsent | undefined; // Declare as global var
+}
+
+export {}; // This line is necessary to make the file a module
