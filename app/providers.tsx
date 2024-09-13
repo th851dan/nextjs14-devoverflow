c1ace72c6bf44a0c6fc3cb503bef24176af3a046
Thanh Son Dang <sondang3298_hp@yahoo.com.vn>
2024-09-13 17:34:24 +0200

    summarize react import

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,6 @@
 // app/providers.js
 'use client'
-import { useEffect } from 'react'
-import React from "react";
+import React, { useEffect } from 'react'
 import posthog from 'posthog-js'
 import { PostHogProvider } from 'posthog-js/react'
 import { useCookiebotCallbacks } from '@/lib/cookiebot'
