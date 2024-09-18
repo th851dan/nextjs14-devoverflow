46461877ea5079a22bd38b1458d41cd0c249f29a
Thanh Son Dang <sondang3298_hp@yahoo.com.vn>
2024-09-18 13:06:19 +0200

    tag badges colorful with primary gradient

--- components/shared/RenderTag.tsx
+++ components/shared/RenderTag.tsx
@@ -12,7 +12,7 @@ interface Props {
 const RenderTag = ({ _id, name, totalQuestions, showCount }: Props) => {
   return (
     <Link href={`/tags/${_id}`} className="flex justify-between gap-2">
-      <Badge className="subtle-medium text-light400_light500 rounded-md border-primary-100 px-4 py-2 uppercase">
+      <Badge className="subtle-medium primary-gradient !text-dark-900 rounded-md px-4 py-2 uppercase">
         {"#" + name}
       </Badge>
       {showCount && (
