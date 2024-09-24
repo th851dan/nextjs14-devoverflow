ccea74c8215247be31f2c05c6a33025cc0d3cf13
Thanh Son Dang <sondang3298_hp@yahoo.com.vn>
2024-09-24 17:18:55 +0200

    styling cookie consent banner

--- components/cookie/cookie-consent-banner.tsx
+++ components/cookie/cookie-consent-banner.tsx
@@ -6,6 +6,7 @@ import { X } from 'lucide-react'
 import { CookieConsentData } from '@/lib/actions/shared.types'
 import { generateUniqueID, getCookie, saveConsentToDatabase, setCookie } from '@/lib/cookie'
 import { usePostHog } from 'posthog-js/react'
+import Image from 'next/image'
 
 const COOKIE_NAME = 'CookieConsent'
 
@@ -68,11 +69,19 @@ export default function CookieConsentBanner() {
             animate={{ y: 0, opacity: 1 }}
             exit={{ y: 100, opacity: 0 }}
             transition={{ duration: 0.5 }}
-            className="fixed bottom-0 left-0 right-0 z-50 p-4 bg-gray-100 shadow-lg dark:bg-gray-800"
+            className="fixed bottom-0 left-0 right-0 z-50 p-4 bg-gray-100 shadow-lg dark:bg-gray-800 "
         >
-            <div className="container mx-auto max-w-screen-xl">
+            <div className="container mx-1 max-w-screen-xl">
                 <div className="flex flex-col sm:flex-row items-center justify-between gap-4">
-                    <div className="flex-1">
+                    <div className="flex-shrink-0">
+                        <Image
+                            src="/assets/images/favicon.svg"
+                            alt="BuddyKnows Logo"
+                            width={80}
+                            height={80}
+                        />
+                    </div>
+                    <div>
                         <h2 className="text-lg font-semibold text-gray-800 dark:text-gray-200">We value your privacy</h2>
                         <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
                             We use cookies to enhance your browsing experience, serve personalized ads or content, and analyze our traffic. By clicking "Accept All", you consent to our use of all cookies. By clicking "Accept Necessary", you consent to only necessary cookies.
@@ -81,20 +90,20 @@ export default function CookieConsentBanner() {
                     <div className="flex items-center gap-4">
                         <button
                             onClick={handleAcceptNecessary}
-                            className="px-4 py-2 text-sm font-medium text-gray-800 bg-gray-200 rounded-md hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 dark:bg-gray-700 dark:text-gray-200 dark:hover:bg-gray-600"
+                            className="px-4 py-2 text-sm font-medium text-gray-800 bg-gray-200 rounded-md hover:bg-primary-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 dark:bg-gray-700 dark:text-gray-200 dark:hover:bg-primary-500 dark:hover:text-white border border-primary-500"
                         >
                             Accept Necessary
                         </button>
                         <button
                             onClick={handleAcceptAll}
-                            className="px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                            className="px-4 py-2 text-sm font-medium text-white primary-gradient rounded-md "
                         >
                             Accept All
                         </button>
                     </div>
                     <button
                         onClick={handleAcceptNecessary}
-                        className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
+                        className="absolute top-2 right-2 text-primary-500 hover:text-primary-100 dark:hover:text-primary-100"
                         aria-label="Close"
                     >
                         <X size={24} />
